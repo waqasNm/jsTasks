7e7c84223943358db056fd1665524f929b962247
waqasnaeem <waqasnaeem91@gmail.com>
2017-06-03 14:50:06 +0500

    updated todo

--- lab-May-26/todo.js
+++ lab-May-26/todo.js
@@ -3,15 +3,36 @@
 
 function todo(){
     var task = document.getElementById('task').value;
-    var date = document.getElementById('date').value;
-    var time = document.getElementById('time').value;
+    document.getElementById('task').value = "";
 
     var newLi = document.createElement('li');
+    var textLi = document.createTextNode( task );
+    
+    var span = document.createElement('SPAN');
+    var spanTxt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.setAttribute('onclick','del(this)');
 
-    var textLi = document.createTextNode( task + "  Date " + date + "  Time " + time);
+    span.appendChild(spanTxt);
 
     newLi.appendChild(textLi);
+    newLi.appendChild(span);
 
     document.getElementById('todo-list').appendChild(newLi);
 
+}
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.setAttribute('onclick','del(this)');
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+function del(x){
+    x.parentNode.remove();
 }
